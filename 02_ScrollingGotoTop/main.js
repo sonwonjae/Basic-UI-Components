6201e3bc837531f2a333f7fe0b273cb1adf33db6
sonwonjae <sonwj091552@gmail.com>
2021-10-11 16:15:56 +0900

    feat: Use lodash library for throttling

--- 02_ScrollingGotoTop/main.js
+++ 02_ScrollingGotoTop/main.js
@@ -2,24 +2,29 @@
 const $scrollIcon = document.querySelector('.scroll-icon');
 
 // throttle
-const throttle = (callback, delay) => {
-  let timerId;
-
-  return event => {
-    if (timerId) return;
-    timerId = setTimeout(() => {
-      callback(event);
-      timerId = null;
-    }, delay);
-  };
-};
+// const throttle = (callback, delay) => {
+//   let timerId;
+//   return event => {
+//     if (timerId) return;
+//     timerId = setTimeout(() => {
+//       callback(event);
+//       timerId = null;
+//     }, delay);
+//   };
+// };
 
 // Event bindings
+/**
+ * use lodash.throttle
+ */
 window.addEventListener(
   'scroll',
-  throttle(() => {
+  _.throttle(() => {
     $scrollIcon.style.display = window.scrollY >= 100 ? 'initial' : 'none';
   }, 200),
+  // throttle(() => {
+  //   $scrollIcon.style.display = window.scrollY >= 100 ? 'initial' : 'none';
+  // }, 200),
 );
 
 $scrollIcon.onclick = () => {
