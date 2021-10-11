902add3d10778535d33027d5e715fb9ddce19abd
sonwonjae <sonwj091552@gmail.com>
2021-10-11 17:33:39 +0900

    feat: implement practice for analog clock

---
+++ 04_AnalogClock/README.md
@@ -0,0 +1,13 @@
+# 4. Analog Clock
+
+![Analog Clock](https://poiemaweb.com/assets/fs-images/exercise/analog-clock.gif)
+
+## 요구사항
+
+- CSS 변수를 사용해 시계의 시침(.hand.hour 요소), 분침(.hand.minute 요소), 초침(.hand.second 요소)을 1초 간격으로 회전시켜 현재 시간을 표시한다.
+- CSS 변수(CSS custom property): Updating a CSS Variable with JavaScript
+
+## TODO
+
+1. css 변수 `--deg`를 활용하여 JS로 `Analog Clock` 조정
+2. `Timeout` function 사용하여 ms 조정
---
+++ 04_AnalogClock/main.js
@@ -0,0 +1,16 @@
+const movementTime = (() => {
+  const $hour = document.querySelector('.hour');
+  const $minute = document.querySelector('.minute');
+  const $second = document.querySelector('.second');
+
+  return (now = new Date()) => {
+    const [hours, minutes, seconds] = [now.getHours(), now.getMinutes(), now.getSeconds()];
+
+    $second.style.setProperty('--deg', seconds * 6);
+    $minute.style.setProperty('--deg', minutes * 6 + (seconds / 60) * 6);
+    $hour.style.setProperty('--deg', hours * 30 + (minutes / 60) * 6 + (seconds / 60 / 60) * 6);
+  };
+})();
+
+// Event binding
+window.addEventListener('DOMContentLoaded', () => setInterval(movementTime, 1000));
