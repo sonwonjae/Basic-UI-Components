ef8953a37fc3e84dfc35fa9eea315732754c2feb
sonwonjae <sonwj091552@gmail.com>
2021-10-12 11:02:40 +0900

    feat: implement practice for dark mode

--- 06_DarkMode/main.js
+++ 06_DarkMode/main.js
@@ -4,9 +4,4 @@ const $toggleButton = document.querySelector('.toggle-button');
 // Event binding
 $toggleButton.onclick = () => {
   localStorage.setItem('dark', document.body.classList.toggle('dark'));
-  console.log(localStorage.getItem('dark'));
 };
-
-// window.addEventListener('DOMContentLoaded', () => {
-//   document.body.classList.toggle('dark', localStorage.getItem('dark') === 'true');
-// });
---
+++ 06_DarkMode/setTheme.js
@@ -0,0 +1,6 @@
+const manualDark = localStorage.getItem('dark');
+const darkMatcher = window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+manualDark
+  ? document.body.classList.toggle('dark', manualDark === 'true')
+  : document.body.classList.toggle('dark', darkMatcher);
