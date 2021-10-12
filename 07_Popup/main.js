92c7c2aef47673979dd5a98318ada2e83490e3e6
Hoya-kim <1203play@gmail.com>
2021-10-12 13:16:41 +0900

    feat: implement practice for popup modal

---
+++ 07_Popup/main.js
@@ -0,0 +1,34 @@
+// DOM nodes
+const $popupButton = document.querySelector('.toggle-popup');
+const $modal = document.querySelector('.dialog-outer');
+const $message = document.querySelector('.popup-message');
+const $form = document.querySelector('.dialog-form');
+const $input = document.querySelector('.dialog-input');
+const $cancle = document.querySelector('.cancle');
+const $closeButton = document.querySelector('.dialog-close-button');
+
+const openModal = () => {
+  $modal.classList.add('is-active');
+  $input.focus();
+};
+
+const closeModal = () => {
+  $modal.classList.remove('is-active');
+  $input.value = '';
+};
+
+// Event bindings
+$popupButton.onclick = openModal;
+
+$modal.onclick = e => {
+  if (e.target !== $modal) return;
+  closeModal();
+};
+
+$form.onsubmit = e => {
+  e.preventDefault();
+  $message.textContent = `from popup: ${$input.value}`;
+  closeModal();
+};
+
+[$cancle, $closeButton].forEach($el => $el.addEventListener('click', closeModal));
