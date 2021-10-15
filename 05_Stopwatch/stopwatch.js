8c16da8082a25a5cfaec5ee9ac0fc77066607a1c
Hoya-kim <1203play@gmail.com>
2021-10-15 13:39:37 +0900

    refactor: organize code with module & closure for stopwatch

--- 05_Stopwatch/stopwatch.js
+++ 05_Stopwatch/stopwatch.js
@@ -1,31 +1,61 @@
-// states
-let measureTime = 0;
-const lapCounter = (() => {
+// helper
+/**
+ * @param {number}:time
+ * @returns {string}
+ */
+const toDigitTime = time => `${time < 10 ? '0' : ''}${time}`;
+
+/**
+ * @param {number}:time
+ * @returns {string}
+ */
+const parseTime = time => {
+  const minutes = toDigitTime(Math.floor(time / 100 / 60) % 60);
+  const seconds = toDigitTime(Math.floor(time / 100) % 60);
+  const milliSec = toDigitTime(time % 100);
+  return `${minutes}:${seconds}:${milliSec}`;
+};
+
+/**
+ * @return {object} About Stopwatch's methods
+ * */
+const stopWatch = (() => {
+  /** @type {number} */
   let count = 0;
 
+  /** @type {number} */
+  let measureTime = 0;
+
+  /** @type {intervalID} */
+  let interval;
+
+  /** @type {boolean} */
+  let running = false;
+
   return {
+    getStatus() {
+      return !running;
+    },
+    updateTime() {
+      measureTime += 1;
+      return parseTime(measureTime);
+    },
+    start(predicate, time) {
+      interval = setInterval(predicate, time);
+      running = true;
+    },
+    stop() {
+      clearInterval(interval);
+      running = false;
+    },
     record() {
       return ++count;
     },
     reset() {
       count = 0;
+      measureTime = 0;
     },
   };
 })();
 
-const parseTime = time => (time < 10 ? '0' + time : '' + time);
-
-const updateTime = () => {
-  measureTime += 1;
-  const minutes = parseTime(Math.floor((measureTime / 100 / 60) % 60));
-  const seconds = parseTime(Math.floor((measureTime / 100) % 60));
-  const milliSec = parseTime(Math.floor(measureTime % 100));
-  return `${minutes}:${seconds}:${milliSec}`;
-};
-
-const resetTime = () => {
-  measureTime = 0;
-  lapCounter.reset();
-};
-
-export { lapCounter, updateTime, resetTime };
+export default stopWatch;
--- 05_Stopwatch/view.js
+++ 05_Stopwatch/view.js
@@ -1,64 +1,62 @@
-import { lapCounter, updateTime, resetTime } from './stopwatch.js';
-
-// state
-let interval;
-let running = false;
+import stopWatch from './stopwatch.js';
 
 // DOM Nodes
-export const [$leftButton, $rightButton] = document.querySelectorAll('.control');
+const [$mainButton, $subButton] = document.querySelectorAll('.control');
 const $display = document.querySelector('.display');
 const $laps = document.querySelector('.laps');
 
-const renderUpdateTime = () => {
-  $display.textContent = updateTime();
-};
-
-const renderStartWatch = () => {
-  $rightButton.removeAttribute('disabled');
-  [$leftButton.textContent, $rightButton.textContent] = ['Stop', 'Lap'];
-  interval = setInterval(renderUpdateTime, 10);
-  running = true;
-};
-
-const renderStopWatch = () => {
-  [$leftButton.textContent, $rightButton.textContent] = ['Start', 'Reset'];
-  clearInterval(interval);
-  running = false;
-};
-
-const makeLapsDiv = value => {
+// helper
+/**
+ * make division element
+ * @param {any}
+ * @return {Element}
+ */
+const makeDiv = value => {
   const $div = document.createElement('div');
   $div.innerHTML = value;
   return $div;
 };
 
-const renderRecordLap = () => {
-  $laps.style.display = 'grid';
-  const $fragment = new DocumentFragment();
-  $fragment.appendChild(makeLapsDiv(lapCounter.record()));
-  $fragment.appendChild(makeLapsDiv($display.textContent));
-  $laps.appendChild($fragment);
+// For cohesion
+const render = {
+  startWatch() {
+    stopWatch.start(() => {
+      $display.textContent = stopWatch.updateTime();
+    }, 10);
+
+    $subButton.removeAttribute('disabled');
+    [$mainButton.textContent, $subButton.textContent] = ['Stop', 'Lap'];
+  },
+  stopWatch() {
+    stopWatch.stop();
+
+    [$mainButton.textContent, $subButton.textContent] = ['Start', 'Reset'];
+  },
+  recordLap() {
+    $laps.style.display = 'grid';
+    const $fragment = new DocumentFragment();
+    $fragment.appendChild(makeDiv(stopWatch.record()));
+    $fragment.appendChild(makeDiv($display.textContent));
+    $laps.appendChild($fragment);
+  },
+  resetWatch() {
+    $subButton.toggleAttribute('disabled', true);
+    $display.textContent = '00:00:00';
+    $laps.innerHTML = `<div class="lap-title">Laps</div>
+                      <div class="lap-title">Time</div>`;
+    $laps.style.display = 'none';
+    stopWatch.reset();
+  },
 };
 
-const renderResetWatch = () => {
-  $rightButton.setAttribute('disabled', 'disabled');
-  $leftButton.textContent = 'Start';
-  $rightButton.textContent = 'Reset';
-  $display.textContent = '00:00:00';
-  $laps.innerHTML = `<div class="lap-title">Laps</div>
-                    <div class="lap-title">Time</div>`;
-  $laps.style.display = 'none';
-  resetTime();
-};
-
-const eventBingding = () => {
-  renderResetWatch();
-  $leftButton.onclick = () => {
-    !running ? renderStartWatch() : renderStopWatch();
+const initStopWatch = () => {
+  render.resetWatch();
+  $mainButton.onclick = () => {
+    stopWatch.getStatus() ? render.startWatch() : render.stopWatch();
   };
-  $rightButton.onclick = () => {
-    !running ? renderResetWatch() : renderRecordLap();
+  $subButton.onclick = () => {
+    stopWatch.getStatus() ? render.resetWatch() : render.recordLap();
   };
 };
 
-export { eventBingding };
+export default initStopWatch;
