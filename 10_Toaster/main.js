da8eb269e32c5cd96c5c63d098fb60327eb243e4
sonwonjae <sonwj091552@gmail.com>
2021-10-15 16:44:14 +0900

    refactor: organize for toaster

--- 10_Toaster/main.js
+++ 10_Toaster/main.js
@@ -1,43 +1,47 @@
-const toaster = {
-  state: [],
-  time: 0,
-  height: 0,
-  createToast({ type, title, message }) {
+const toaster = (() => {
+  const toasts = [];
+
+  const createToast = ({ type, title, message }) => {
     const template = document.createElement('template');
     template.innerHTML = `<div class="toast toast-${type}"">
-    <h4 class="toast-heading">${title} ${this.state.length}</h4>
-    <div class="toast-message">
-      <svg width="24" height="24">
-        <use xlink:href="#${type}" />
-      </svg>
-      <p>${message}</p>
-    </div>
-    <a class="close">&times;</a>
-  </div>`;
+     <h4 class="toast-heading">${title} ${toasts.length}</h4>
+     <div class="toast-message">
+       <svg width="24" height="24">
+         <use xlink:href="#${type}" />
+       </svg>
+       <p>${message}</p>
+     </div>
+     <a class="close">&times;</a>
+   </div>`;
 
     return template.content.firstElementChild;
-  },
-  add({ type, title, message }) {
-    const newToast = this.createToast({ type, title, message });
+  };
 
-    this.state.push(newToast);
-    document.body.appendChild(newToast);
-
-    const toastHeight = +getComputedStyle(newToast)
+  const liftupToasts = toast => {
+    const toastHeight = +getComputedStyle(toast)
       .getPropertyValue('--toast-height')
       .replace(/[^0-9]/g, '');
 
-    this.state.forEach($el => {
+    toasts.forEach($el => {
       $el.style.bottom = `${+$el.style.bottom.replace(/[^0-9]/g, '') + toastHeight}px`;
     });
+  };
 
-    newToast.style.bottom = 0;
+  return {
+    add(toastInfo) {
+      const newToast = createToast(toastInfo);
+      toasts.push(newToast);
+      document.body.appendChild(newToast);
 
-    setTimeout(() => {
-      document.body.removeChild(this.state.shift());
-    }, 3000);
-  },
-};
+      liftupToasts(newToast);
+      newToast.style.bottom = 0;
+
+      setTimeout(() => {
+        document.body.removeChild(toasts.shift());
+      }, 3000);
+    },
+  };
+})();
 
 const TOAST_TYPE = {
   SUCCESS: 'success',
