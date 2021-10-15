7a63f41b463f98eb827b6cd95c250eb3586b8a08
sonwonjae <sonwj091552@gmail.com>
2021-10-15 14:29:17 +0900

    refactor: organize for modal & destructuring closebutton click

--- 07_Popup/main.js
+++ 07_Popup/main.js
@@ -1,34 +1,36 @@
-// DOM nodes
-const $popupButton = document.querySelector('.toggle-popup');
-const $modal = document.querySelector('.dialog-outer');
-const $message = document.querySelector('.popup-message');
-const $form = document.querySelector('.dialog-form');
-const $input = document.querySelector('.dialog-input');
-const $cancle = document.querySelector('.cancle');
-const $closeButton = document.querySelector('.dialog-close-button');
+(() => {
+  // DOM nodes
+  const $popupButton = document.querySelector('.toggle-popup');
+  const $modal = document.querySelector('.dialog-outer');
+  const $message = document.querySelector('.popup-message');
+  const $form = document.querySelector('.dialog-form');
+  const $input = document.querySelector('.dialog-input');
+  const $cancel = document.querySelector('.cancel');
+  const $closeButton = document.querySelector('.dialog-close-button');
 
-const openModal = () => {
-  $modal.classList.add('is-active');
-  $input.focus();
-};
+  // modal control function
+  const openModal = () => {
+    $modal.classList.add('is-active');
+    $input.focus();
+  };
 
-const closeModal = () => {
-  $modal.classList.remove('is-active');
-  $input.value = '';
-};
+  const closeModal = () => {
+    $modal.classList.remove('is-active');
+    $input.value = '';
+  };
 
-// Event bindings
-$popupButton.onclick = openModal;
+  // Event bindings
+  $popupButton.onclick = openModal;
+  [$cancel.onclick, $closeButton.onclick] = [closeModal, closeModal];
 
-$modal.onclick = e => {
-  if (e.target !== $modal) return;
-  closeModal();
-};
+  $modal.onclick = e => {
+    if (e.target !== $modal) return;
+    closeModal();
+  };
 
-$form.onsubmit = e => {
-  e.preventDefault();
-  $message.textContent = `from popup: ${$input.value}`;
-  closeModal();
-};
-
-[$cancle, $closeButton].forEach($el => $el.addEventListener('click', closeModal));
+  $form.onsubmit = e => {
+    e.preventDefault();
+    $message.textContent = `from popup: ${$input.value}`;
+    closeModal();
+  };
+})();
