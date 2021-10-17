59d2e4b8f98317538047ee6e7a5ca9a82082f1ab
Hoya-kim <1203play@gmail.com>
2021-10-17 21:38:25 +0900

    refactor: fix machanism for dark mode

--- 06_DarkMode/main.js
+++ 06_DarkMode/main.js
@@ -1,8 +1,22 @@
 (() => {
+  document.body.style.visibility = 'hidden';
   // DOM Nodes
   const $toggleButton = document.querySelector('.toggle-button');
 
   // Event binding
+  window.addEventListener('DOMContentLoaded', () => {
+    const manualDark = localStorage.getItem('dark');
+    const darkMatcher = window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+    manualDark
+      ? document.body.classList.toggle('dark', manualDark === 'true')
+      : document.body.classList.toggle('dark', darkMatcher);
+  });
+
+  window.ontransitionend = () => {
+    document.body.style.visibility = 'visible';
+  };
+
   $toggleButton.onclick = () => {
     localStorage.setItem('dark', document.body.classList.toggle('dark'));
   };
